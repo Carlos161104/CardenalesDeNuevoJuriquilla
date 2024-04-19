89e720ecd62154f9b0a879bd622d9db6b59616e7
Carlos161104 <canoja161104@mail.com>
2024-04-19 16:41:20 -0600

    Si

--- Andenes/App.jsx
+++ Andenes/App.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
+  Modal,
+  ScrollView
 } from 'react-native';
 
 const App= () => {
-  
+  const [modalInicioSecion, setmodalInicioSecion] = useState(false);
   return (
-    <Text>Roma de la come</Text>
+    <View
+    style={styles.contenedor}>
+      <Text>Roman de la come</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  contenedor: {
+    backgroundColor: '#15303B',
+    flex: 1
+  }
 });
 
 export default App;
