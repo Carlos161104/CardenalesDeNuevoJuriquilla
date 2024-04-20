622d1ba650d58fee1117f6f638728f0230210755
Carlos161104 <canoja161104@mail.com>
2024-04-19 19:38:30 -0600

    Nuevo componenete Login

---
+++ Andenes/src/components/menu.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View,
+    Modal,
+    TextInput,
+    Pressable,
+    Image
+  } from 'react-native';
+
+const menu= () => {
+  return (
+    <Text>
+        
+    </Text>
+  )
+}
+const styles = StyleSheet.create({
+})
+
+export default menu
